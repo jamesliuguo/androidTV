cf8fe04b8bb29e9ba74fdee5e1fb2a8dcfc2e928
v <v@localhost.localdomain>
2013-06-10 09:46:12 +0800

    unittest framework

---
+++ a/src/com/v/tv/NetClient.java
@@ -0,0 +1,16 @@
+package com.v.tv;
+
+public class NetClient {
+	public void ownloadFile()
+	{
+		
+	}
+	public void Post(String url, String vars)
+	{
+		
+	}
+	 public static void main(String[] args) { 
+		 System.out.print("ok");
+	 }
+	 
+}
---
+++ a/src/com/v/tv/test.java
@@ -0,0 +1,10 @@
+package com.v.tv;
+
+import junit.framework.TestCase;
+
+public class test extends TestCase {
+	public void test()
+	{
+		System.out.print("test");
+	}
+}
