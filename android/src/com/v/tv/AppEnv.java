44a651be69de3087e7ec61a3883995f24be8aae9
root <root@localhost.localdomain>
2013-06-22 17:02:40 +0800

    change for saving

--- android/src/com/v/tv/AppEnv.java
+++ android/src/com/v/tv/AppEnv.java
@@ -1,8 +1,42 @@
 package com.v.tv;
 
+import android.os.Environment;
+
 public class AppEnv {
+	private static String localCachePath = null;
+	private static String getLocalPath()
+	{
+		if (localCachePath != null) return localCachePath;
+		
+	   //File path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+	   String err = null;
+	   try{
+		   localCachePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+	   }
+	   catch(Exception e) {
+		   err = e.getMessage();
+	   }
+	   return localCachePath;
+	}
+	private static String getNameOnly(String url)
+	{
+		String str = "";
+		int index = url.lastIndexOf("/");
+		if (index >=0)	str = url.substring(index+1);
+		return str;
+	}
 	static public String getLocalFile(String url)
 	{
-		return "";
+		String str = getLocalPath() + getNameOnly(url);
+		return str;
+	}
+	/* Checks if external storage is available to at least read */
+	public static boolean isExternalStorageReadable() {
+	    String state = Environment.getExternalStorageState();
+	    if (Environment.MEDIA_MOUNTED.equals(state) ||
+	        Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+	        return true;
+	    }
+	    return false;
 	}
 }
--- android/src/com/v/tv/Configuration.java
+++ android/src/com/v/tv/Configuration.java
@@ -3,7 +3,7 @@ package com.v.tv;
 public class Configuration {
 	//the first item
 	public static  String ServerURL = "http://192.168.1.1/";
-
+	public static String  clientName = "Client1";
 	public static boolean AnalyzeClientConfig()
 	{
 		return true;
@@ -17,5 +17,6 @@ public class Configuration {
 		return true;
 	}
 	
+	//
 	public static String[] movieFiles = null;
 }
